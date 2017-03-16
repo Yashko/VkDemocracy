d7fc82dd05ce75678239bf8f334f39c176e32a9a
Yashko <yashkome@gmail.com>
2017-03-16 23:10:48 +0200

    v1.01

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-'use strict';
 const VK = require('simplevk');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const config = require('./config.json');
 
-let SETTINGS = {
-    watch: '-123',
-    token: '123qwe',
-    time: [60, 60, 60], //in seconds
-    likes: [1, 2, 3], 
-    secret: '', //Secret key from callback api settings group page 
-    port: 3003,
-    confirm: '' //confirmation code to verify the server endpoint
-};
+if (config.time.length != config.likes.length) throw new Error("config error");
 
-if (SETTINGS.time.length != SETTINGS.likes.length) throw new Error("Settings error");
-
-VK.setToken(SETTINGS.token);
+VK.setToken(config.token);
 VK.setLang('ru');
 VK.setVersion('5.33');
 
@@ -27,11 +17,11 @@ VK.events.on('httpError', function(err) {
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
-    if (req.body.type == 'confirmation') return res.send(SETTINGS.confirm);
-    if (req.body.secret && (req.body.secret != SETTINGS.secret)) return console.log('wrong secret');
+    if (req.body.type == 'confirmation') return res.send(config.confirm);
+    if (req.body.secret && (req.body.secret != config.secret)) return console.log('wrong secret');
     res.send('ok');
     if (req.body.type != "wall_reply_new") return; //ignoring all events, expect the new comment event
-    if (unixtime > (req.body.object.date + SETTINGS.time[0])) return; //expired
+    if (unixtime > (req.body.object.date + config.time[0])) return; //expired
     let obj = req.body.object;
     let comment = {
         id: obj.id,
@@ -42,12 +32,12 @@ app.post('/', function(req, res) {
     }
     setTimeout(function() {
         APPDATA.checkComment(comment)
-    }, SETTINGS.time[comment.loop] * 1000);
+    }, config.time[comment.loop] * 1000);
     
 });
 
-app.listen(SETTINGS.port, function() {
-    console.log('VK democracy started. port ' + SETTINGS.port);
+app.listen(config.port, function() {
+    console.log('VK democracy started. port ' + config.port);
 });
 
 let APPDATA = {
@@ -55,7 +45,7 @@ let APPDATA = {
         APPDATA.queue.tasks.push({
             method: "wall.getComments",
             args: {
-                owner_id: SETTINGS.watch,
+                owner_id: config.watch,
                 post_id: comment.post_id,
                 start_comment_id: comment.id,
                 count: 1,
@@ -64,11 +54,11 @@ let APPDATA = {
             callback: function(err, res) { 
                 if (err) return console.log(err);
                 if (!res) return;
-                if ((res.response.items.length == 0) || (res.response.items[0].likes.count < SETTINGS.likes[comment.loop])) {
+                if ((res.response.items.length == 0) || (res.response.items[0].likes.count < config.likes[comment.loop])) {
                     APPDATA.queue.tasks.push({
                         method: "wall.deleteComment",
                         args: {
-                            owner_id: SETTINGS.watch,
+                            owner_id: config.watch,
                             comment_id: comment.id
                         },
                         callback: function(err, res) {
@@ -77,11 +67,11 @@ let APPDATA = {
                         }
                     })
                 } else {
-                	if (comment.loop == (SETTINGS.time.length-1)) return;
+                	if (comment.loop == (config.time.length-1)) return;
 			comment.loop += 1;
 			setTimeout(function() {
 				APPDATA.checkComment(comment)
-			}, SETTINGS.time[comment.loop] * 1000);
+			}, config.time[comment.loop] * 1000);
                 }
             }
         });
@@ -104,19 +94,5 @@ let APPDATA = {
 }
 
 let unixtime = () => { return Date.now() / 1000 | 0; }
-
-function test() {
-    APPDATA.queue.tasks.push({
-        method: "wall.get",
-        args: {
-		    owner_id: SETTINGS.watch,
-		    count: SETTINGS.count
-		},
-        callback: function(err, res) {
-            if (err) return console.log(err);
-		    return console.log(res);
-        }
-    })
-}
 
 APPDATA.queue.timeout.set();
